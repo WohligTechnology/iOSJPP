114d019002dd3c79405ebde8fe1d60ca33ff91a7
Chintan <chintan@wohlig.com>
2016-01-25 17:22:31 +0530

    fonts added constrains remaining

---
+++ JPP/news.swift
@@ -0,0 +1,21 @@
+//
+//  news.swift
+//  JPP
+//
+//  Created by Chintan Shah on 25/01/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+class news: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
