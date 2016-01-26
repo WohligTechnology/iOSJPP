d0cbb61421958e3d77db642977beec60a35e9f28
TusharSachde <tushar@wohlig.com>
2016-01-26 18:43:04 +0530

    About Done

---
+++ JPP/about.swift
@@ -0,0 +1,35 @@
+//
+//  schedule.swift
+//  JPP
+//
+//  Created by Chintan Shah on 25/01/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class about: UIView {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "about", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+    }
+    
+    
+    
+}
