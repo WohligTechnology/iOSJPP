0ff58f3bb070f1276646fcac5487dcf98584ed43
Chintan <chintan@wohlig.com>
2016-01-26 22:41:06 +0530

    changes for the points

--- JPP/PointTableController.swift
+++ JPP/PointTableController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class PointTableController: UIViewController {
+class PointTableController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableHeader: table!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNavigationBarItemText("POINT TABLE")
+        tableHeader.tableNo.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tableNo.text = "No."
+        
+        
+        tableHeader.tableTeam.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tableTeam.text = "Team"
+        
+        
+        tableHeader.tablePlayed.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tablePlayed.text = "P"
+        
+        tableHeader.tableWon.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tableWon.text = "W"
+        
+        tableHeader.tableLost.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tableLost.text = "L"
+        
+        tableHeader.tablePoint.font = UIFont(name:"Oswald-Bold", size: 14.0)
+        tableHeader.tablePoint.text = "PTS"
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +42,24 @@ class PointTableController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.SingleLine
+        tableView.tableFooterView = UIView()
+        let cell = tableView.dequeueReusableCellWithIdentifier("pointTableCell", forIndexPath: indexPath)
+        cell.addSubview(table(frame: CGRectMake(0,0,cell.frame.width,cell.frame.height)))
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    
     
 
     /*
