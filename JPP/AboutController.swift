057c5a5734bdecba1353f7c80dd205996c22e710
Chintan <chintan@wohlig.com>
2016-01-26 23:54:59 +0530

    about is also done now.

--- JPP/AboutController.swift
+++ JPP/AboutController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class AboutController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var verticalLayout : VerticalLayout!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNavigationBarItemText("ABOUT US")
+        
+        
+        self.verticalLayout = VerticalLayout(width: self.view.frame.width);
+        
+        self.verticalLayout.addSubview(about(frame: CGRectMake(8,8,verticalLayout.frame.width-16,300)));
+        
+        self.verticalLayout.addSubview(trapezium(frame: CGRectMake(8,8,verticalLayout.frame.width-16,34)))
+        
+        
+        
+        self.verticalLayout.addSubview(team(frame: CGRectMake(8,0,verticalLayout.frame.width-16,150)))
+        
+        self.verticalLayout.addSubview(team(frame: CGRectMake(8,0,verticalLayout.frame.width-16,150)))
+        
+        resizeView(8)
+        
+        
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func resizeView(offset:CGFloat)
+    {
+        self.verticalLayout.layoutSubviews()
+        self.scrollView.contentSize = CGSize(width: self.verticalLayout.frame.width, height: self.verticalLayout.frame.height + offset)
+        self.scrollView.insertSubview(self.verticalLayout, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
