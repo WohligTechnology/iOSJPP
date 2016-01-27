19b9b6b542956aad9a58dfc15a746b00685b2ddb
Harsh Thakkar <harshthakkar46@gmail.com>
2016-01-27 15:17:56 +0530

    changes

---
+++ JPP/thumbnailImage.swift
@@ -0,0 +1,45 @@
+//
+//  thumbnailImage.swift
+//  JPP
+//
+//  Created by Chintan Shah on 27/01/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class thumbnailImage: UIView {
+    
+    @IBOutlet weak var playerImage: player!
+    @IBOutlet weak var playerName: player!
+    @IBOutlet weak var playerPosition: player!
+    @IBOutlet weak var playerNationality: UILabel!
+    
+    @IBOutlet weak var playerBirth: UILabel!
+    @IBOutlet weak var playerJerseyno: UILabel!
+    @IBOutlet weak var playerDesc: UILabel!
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "thumbnailImage", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+    }
+    
+    
+    
+}
