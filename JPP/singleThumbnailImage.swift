d0cde6a1f07c44f9e3619fc0d93ccd81887b867e
Harsh Thakkar <harshthakkar46@gmail.com>
2016-01-27 16:29:15 +0530

    views added

---
+++ JPP/singleThumbnailImage.swift
@@ -0,0 +1,41 @@
+//
+//  singleThumbnailImage.swift
+//  JPP
+//
+//  Created by Chintan Shah on 27/01/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class singleThumbnailImage: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "singleThumbnailImage", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+    }
+    
+//    func shareImage() {
+//        var img: UIImage = UIImage(named: "")!
+//        var shareItems:Array = [img]
+//        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+//        activityViewController.excludedActivityTypes = [UIActivityTypePrint, UIActivityTypePostToWeibo, UIActivityTypeCopyToPasteboard, UIActivityTypeAddToReadingList, UIActivityTypePostToVimeo]
+//        self.presentViewController(activityViewController, animated: true, completion: nil)
+//    }
+    
+    
+}
