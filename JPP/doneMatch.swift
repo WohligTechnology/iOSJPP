6ae9b7c0645c1ddc4dc7df7e1a5e6eb6d32db8ea
Harsh Thakkar <harshthakkar46@gmail.com>
2016-02-09 12:13:55 +0530

    done match view added and fonts added

---
+++ JPP/doneMatch.swift
@@ -0,0 +1,46 @@
+//
+//  doneMatch.swift
+//  JPP
+//
+//  Created by Chintan Shah on 09/02/16.
+//  Copyright © 2016 Wohlig Technology. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class doneMatch: UIView {
+    
+    @IBOutlet var teamOneScore: UILabel!
+    @IBOutlet var teamTwoScore: UILabel!
+    let borderWidth:CGFloat = 3
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+        
+    }
+    
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "doneMatch", bundle: bundle)
+        let sortnewview = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        sortnewview.frame = bounds
+        sortnewview.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        self.addSubview(sortnewview);
+        
+        // font and border to score
+        self.teamOneScore.font = UIFont(name: "Kenyan-Coffee", size: 45)
+        self.teamTwoScore.font = UIFont(name: "Kenyan-Coffee", size: 45)
+        self.teamOneScore.layer.borderWidth = borderWidth
+        self.teamOneScore.layer.borderColor = UIColor.whiteColor().CGColor
+        self.teamTwoScore.layer.borderWidth = borderWidth
+        self.teamTwoScore.layer.borderColor = UIColor.whiteColor().CGColor
+        
+    }
+    
+}
