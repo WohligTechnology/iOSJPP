03ad35392afbd25bf9bbcf49b7173c9375238e0b
Harsh Thakkar <harshthakkar46@gmail.com>
2016-02-11 11:44:52 +0530

    some players information added

--- JPP/KnowTeamController.swift
+++ JPP/KnowTeamController.swift
@@ -45,7 +45,7 @@ class KnowTeamController: UIViewController,UICollectionViewDataSource,UICollecti
     func collectionView(collectionView : UICollectionView,layout collectionViewLayout:UICollectionViewLayout,sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
     {
         
-        return CGSizeMake(self.view.frame.width/2 - 4 ,self.view.frame.width/2)
+        return CGSizeMake(self.view.frame.width/2 - 4, self.view.frame.width/2)
     }
     
     func collectionView(collectionView: UICollectionView,
